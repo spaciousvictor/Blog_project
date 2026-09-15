6e9dcf12a7f59f47daf3c8f1fa2f5389d6fef594
Victor Sunday <vs0316264@gmail.com>
2026-09-15 07:44:02 -0500

    Configure settings and requirements for deployment

--- backend/blog_api/settings.py
+++ backend/blog_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+ak1)+3etxi+i0$&3n%^8xhn!=if3&uox99%9y=^md$%ven=+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
